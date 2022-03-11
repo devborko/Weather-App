0cb43cff121baa1979217a633ab0a630d096c21d
borko <dev.borko@gmail.com>
2022-03-11 22:17:09 +0100

    Defining a Repository and ViewModelFactory

--- app/src/main/java/com/example/weatherapp/Repositories/WeatherRepository.kt
+++ app/src/main/java/com/example/weatherapp/Repositories/WeatherRepository.kt
@@ -4,5 +4,6 @@ import com.example.weatherapp.Networking.WeatherInstance
 import retrofit2.http.Query
 
 class WeatherRepository(private val instance: WeatherInstance) {
-
+    suspend fun getWeather(lat: Double, lon: Double, appid: String)
+        = instance.getWeather(lat,lon,appid)
 }
--- app/src/main/java/com/example/weatherapp/ViewModels/WeatherViewModelFactory.kt
+++ app/src/main/java/com/example/weatherapp/ViewModels/WeatherViewModelFactory.kt
@@ -4,7 +4,10 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.weatherapp.Repositories.WeatherRepository
 
-class WeatherViewModelFactory(val repository: WeatherRepository): ViewModelProvider.NewInstanceFactory() {
+class WeatherViewModelFactory(val repository: WeatherRepository):
+    ViewModelProvider.NewInstanceFactory() {
 
-
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return WeatherViewModel(repository) as T
+    }
 }
